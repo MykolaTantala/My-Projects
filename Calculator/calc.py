41455db84e4bd7d309996fc6c8b1227d7235f1b4
MykolaTantala <87090493+MykolaTantala@users.noreply.github.com>
2021-07-08 00:15:22 +0300

    Update calc.py

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -3,30 +3,30 @@ import math
 if __name__ == '__main__':
 	print("To start the Floppa Calculator you need to run the file 'main.py'")
 
-# Ця функція округляє числа, щоб після коми не було більше 7 чисел, а також округляє число до цілого числа, якщо різниця між не округленим і округленим числом менша за 1/10000000:
+# This function rounds numbers so that there are no more than 7 numbers after the comma, and also rounds a number to an integer if the difference between a non-rounded and a rounded number is less than 1/10000000:
 def Round(num):
 	if math.ceil(num) - num < 0.0000001 or num - math.floor(num) < 0.0000001:
 		return round(num)
 	else:
 		return round(num, 7)
 
-# Функція, що змінює знак числа на протилежний:
+# Function that changes the sign of the number to the opposite(not yet used in the main code):
 def inv(n):
 	return Round(-n)
 
-#Функція, що визначає корінь із числа. І корінь може бути не тільки квадратним, а й кубічним, і так далі. Для цього я не використав вбудований в модуль math метод, а використав піднесення числа в обернений степінь. math.sqrt(n) - це те саме, що й n**0.5 або n**(1/2), а n**(1/3) - це кубічний корінь з числа n:
+#A function that determines the root of a number. And the root can be not only square but also cubic, and so on. For this purpose I did not use the method built in the math module, and used raising of number in inverse degree. math.sqrt(n) is the same as n**0.5 or n**(1/2), and n**(1/3) is the cubic root of n:
 def nroot(n, r=2):
 	n **= 1/r
 	return Round(n)
 
-# Функція, що визначає факторіал числа:
+# Function that determines the factorial of a number:
 def fact(n):
 	return math.factorial(n)
 
-# Функція, що перетворює відсотки в десяткові дроби:
+# Function that converts interest to decimal fractions:
 def percent(n):
 	return Round(n/100)
 
-# Функція, що перетворює передане їй число на обернений дріб:
+# Function that converts the number passed to it into an inverse fraction:
 def reciproc(n):
 	return Round(1/n)
